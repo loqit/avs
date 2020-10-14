357696bddcea2167f7f5d6d8440e62ce20a6a32b
loqit <bobrandrey1972@gmail.com>
2020-10-14 15:10:12 +0300

    registr fix

--- avs_lab1/main.cpp
+++ avs_lab1/main.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-__int16 conv8to16(__int8* data)
+void conv8to16(__int8* data, __int16* res)
 {
-    __int16* res;
     __asm
     {
         mov eax, data
+        mov ebx, res
 
-        movd mm0, [eax]
-        movd mm1, [eax]
+        movq mm0, [eax]
+        movq mm1, [eax]
 
         pxor mm2, mm2
         pcmpgtb mm2, mm0
@@ -18,99 +18,114 @@ __int16 conv8to16(__int8* data)
         pcmpgtb mm2, mm1
         punpckhbw mm1, mm2
 
-        movd [ebx], mm0
-        movd [ebx + 8], mm1
+        movq [ebx], mm0
+        movq [ebx + 8], mm1
 
-        mov res, ebx
         emms
     }
-    return *res;
 }
 
-__int16 mmx_sum(__int16* a, __int16* b)
+void mmx_sum(__int16* a, __int16* b, __int16* res)
 {
-    __int16* res;
     __asm
     {
+        xor ebx, ebx
+        xor esi, esi
+        xor edi, edi
         mov esi, a
         mov edi, b
+        mov ebx, res
 
         movq mm0, [esi]
         movq mm1, [edi]
         paddsw mm0, mm1
 
+        movq mm2, [esi + 8]
+        movq mm3, [edi + 8]
+        paddsw mm2, mm3
+
         movq [ebx], mm0
-        mov res, ebx
+        movq [ebx + 8], mm2
         emms
     }
-    return *res;
 }
 
-__int16 mmx_sub(__int16* a, __int16* b)
+void mmx_sub(__int16* a, __int16* b, __int16* res)
 {
-    __int16* res;
     __asm
     {
+        xor ebx, ebx
+        xor esi, esi
+        xor edi, edi
         mov esi, a
         mov edi, b
+        mov ebx, res
 
         movq mm0, [esi]
         movq mm1, [edi]
         psubsw mm0, mm1
 
+        movq mm2, [esi + 8]
+        movq mm3, [edi + 8]
+        psubsw mm2, mm3
+
         movq [ebx], mm0
-        mov res, ebx
+        movq [ebx + 8], mm2
+
         emms
     }
-    return *res;
 }
 
-__int16 mmx_mul(__int16* a, __int16* b)
+void mmx_mul(__int16* a, __int16* b, __int16* res)
 {
-    __int16* res;
     __asm
     {
+        xor esi, esi
+        xor edi, edi
+        xor ebx, ebx
         mov esi, a
         mov edi, b
+        mov ebx, res
+
         movq mm0, [esi]
         movq mm1, [edi]
+        pmullw mm0, mm1
 
-        pmulhw mm0, mm1
-        movq mm2, [esi]
-        pmullw mm1, mm2
-        movq mm2, mm0
-        movq mm3, mm1
-        punpckhwd mm3, mm2
-        punpcklwd mm1, mm0
+        movq mm2, [esi + 8]
+        movq mm3, [edi + 8]
+        pmullw mm2, mm3
 
-        movq [ebx], mm1
-        movq [ebx + 32], mm3
-        mov res, ebx
+        movq [ebx], mm0
+        movq [ebx + 8], mm2
+
         emms
     }
-    return *res;
+
 }
 
-void task()
-{
+int main() {
+
     __int8 a[8] = {1, 42, 100, 14, 57, 8, 23, 11};
     __int8 b[8] = {37, 10, 45, 97, 26, 88, 46, 50};
     __int16 c[8] = {54, 16, 129, 32, 95, 211, 11, 187};
     __int16 d[8] = {43, 45, 235, 80, 35, 60, 294, 6};
 
-    for(int i = 0; i < 8; i++)
+    __int16 a16[8], b16[8];
+    conv8to16(a, a16);
+    conv8to16(b, b16);
+
+    __int16 a_b[8];
+    mmx_sum(a16, b16, a_b);
+
+    __int16 ab_c[8];
+    mmx_mul(a_b, c, ab_c);
+
+    __int16 res[8];
+    mmx_sub(ab_c, d, res);
+
+    for(int i = 0;i < 8; i++)
     {
-        __int16 a16 = conv8to16(&a[i]);
-        __int16 b16 = conv8to16(&b[i]);
-        __int16 a_b = mmx_sum(&a16, &b16);
-        __int16 ab_c = mmx_mul(&a_b, &c[i]);
-        __int16 ans = mmx_sub(&ab_c, &d[i]);
-        cout << ans << endl;
+        cout << res[i] << endl;
     }
-}
-
-int main() {
-
-    task();
     return 0;
 }
