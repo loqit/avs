7e90ea6351b1a11a5e191ab06d5154f33107d9aa
loqit <bobrandrey1972@gmail.com>
2020-12-03 21:45:31 +0300

    fix

--- avs_lab3/main.cpp
+++ avs_lab3/main.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
 #include <omp.h>
 #include <chrono>
+#include <ctime>
+#include <random>
+
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
 
 void matrixVector(int n, int m, const double* arr, bool flag)
 {
-    std::cout << "Enter vector" << '\n';
+    //std::cout << "Enter vector" << '\n';
     auto * v = new double [m];
+    //#pragma omp for
     for (int i = 0;i < m;i++)
     {
-        std::cin >> v[i];
+        v[i] = fRand(1,100);
     }
+
+
     double res[n];
-#pragma omp parallel shared(n, m, arr, v, flag) reduction(+:tmp) private(i, j)
+#pragma omp parallel // shared(n, m, arr, v) reduction(+:res)
     {
-#pragma omp for sheldue(static)
+        /*
+        int nthreads = omp_get_num_threads();
+    int threadid = omp_get_thread_num();
+    int items_per_thread = m / nthreads;
+    int lb = threadid * items_per_thread;
+    int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+*/
+#pragma omp for
         for (int i = 0;i < n;i++)
         {
-            double tmp = 0;
+            res[i] = 0;
             for (int j = 0;j < m;j++)
             {
                 int x = i, y = j;
                 if (flag) { std::swap(x, y); }
-                tmp += arr[x * m + y] * v[j];
+#pragma omp atomic
+                res[i] += arr[x * m + y] * v[j];
             }
-            res[i] = tmp;
         }
     }
 
@@ -39,14 +57,21 @@ int main() {
     std::cout << "Enter matrix size:";
     std::cin >> n >> m;
     double a[n][m];
-    std::cout << "Enter matrix" << '\n';
-    for (int i = 0;i < n;i++)
+
+
+
+#pragma omp for
+    for (int i = 0;i < n ;i++)
     {
         for (int j = 0;j < m;j++)
         {
-            std::cin >> a[i][j];
+            a[i][j] = fRand(1,100);
+            // std::cout << a[i][j] << " " ;
         }
+        //std::cout << '\n';
     }
+
+
 
     auto begin = std::chrono::high_resolution_clock::now();
     matrixVector(n, m, &a[0][0], false);
@@ -59,6 +84,6 @@ int main() {
     matrixVector(n, m, &a[0][0], true);
     end = std::chrono::high_resolution_clock::now();
     time = std::chrono::duration<double, std::milli>(end - begin).count();
-    std::cout << "Time : " << time << std::endl;
+    std::cout << '\n' << "Time : " << time << std::endl;
     return 0;
 }
